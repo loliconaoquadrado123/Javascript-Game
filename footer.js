d4130d26020d35017a81b3112e4fe7693d3372ff
loliconaoquadrado123 <68407584+loliconaoquadrado123@users.noreply.github.com>
2020-07-29 18:05:16 -0300

    Atualizando e documentando

--- footer.js
+++ footer.js
@@ -10,6 +10,24 @@ tela = tela.getContext('2d');
 tela.fillStyle = "rgb(200,0,0)"; // define a cor de preenchimento do retângulo
 tela.fillRect(10, 10, 55, 50);
 
+//start all images
 
-tela.drawImage(document.getElementById('player'),100,10);
+img = new Image(100,200);
+img.src='player.jpg';
+//draw all
 
+//KEYS
+
+window.addEventListener('keydown',(event)=>{
+    alert(event.key);
+})
+function move(x,y){
+    
+}
+function frame(x,y,id){
+    tela.drawImage(img,x,y);
+    console.log('a')
+}
+x = 10;
+y = 20;
+setInterval(frame(x,y),1000);
