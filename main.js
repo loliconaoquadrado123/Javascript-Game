b1d0fb77005564bcaba58a56501b1fcb68acd913
loliconaoquadrado123 <68407584+loliconaoquadrado123@users.noreply.github.com>
2020-08-01 12:23:23 -0300

    Update main.js

--- main.js
+++ main.js
@@ -1,9 +1,11 @@
 function load(){
-        document.createElement('div').setAttribute('id','load');
-
-        
+        load = document.createElement('div').setAttribute('id','load');
+        document.load(()=>{
+        load.style.display="none";
+        })
 }
 
 document.onload = ()=>{
     load();
+    setInterval(frame, 100);
 }
