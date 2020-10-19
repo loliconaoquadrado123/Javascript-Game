4918cfe7f2050b16c8be359cb274039823746a89
loliconaoquadrado123 <68407584+loliconaoquadrado123@users.noreply.github.com>
2020-10-19 12:17:42 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,84 +1,99 @@
-/*
-function load(){
-        
-
-document.onload = ()=>{
-    load.style.height=window.innerHeight;
-        document.load(()=>{
-        load.style.display="none";
-        })
-}
-    setInterval(frame, 100);
-}
-
-function img(src){
-    this.src=src;
-}*/
 window.addEventListener('load',function(){
-    x=0;
-    y=0;
-    screenH = window.innerHeight;
-    screen = document.getElementById('screen');
-    screen2d = screen.getContext('2d');
-    player=new Image();
-    player.src='player.jpg';
-    img=player;
-    
-function frame() {
-    
-    screen2d.clearRect(0, 0, screen.width, screen.height);
-    screen2d.beginPath();
-    screen2d.drawImage(img,x,y);
-    screen2d.closePath();
-    
-    
-}
-function verify(key){
-    for(i=0;keysPressed[i]==keysPressed.lengh;i++){
-        if(keysPressed[i]=key){
-            return 1;
-        }else{
-            return 0;
+    class Object{
+        constructor(x,y,width,height,vel,direction,color){
+            this.x=x;
+            this.y=y;
+            this.width=width;
+            this.height=height;
+            this.vel=vel;
+            this.direction=direction;
+            this.color=color;
+            this.sprite=new Image(width,height);
+
         }
     }
-    
-}
-keysPressed=[];
-    window.addEventListener('keydown',function(event){
-        
-        
-        switch(event.key){
+    class ComboKeys{
+        constructor(){
             
-            case 'ArrowUp':
-                
-                key = 'up';
-                if(verify(key)){
-                    keysPressed[].push(key);
-                }
-                y-=1;
-                break;
-            case 'ArrowDown':
-                key = 'down';
-                y+=1;
+        }
+
+    }
+    // class Player extends Object{
         
-                break;
-            case 'ArrowLeft':
-                key = 'left';
-                x-=1;
-                break;
-            case 'ArrowRight':
-                key = 'right';
-                x+=1;
-                break;
-                        
-        }
-})
-window.addEventListener('keyup',function(event){
-        for(i = 0;i==keysPressed.lengh;i++){
-            keysPressed[i]=false;
-        }
+    // mainScene={
+        
+    //     Player:new Object(0,0,100,1000,0,0,"#ffffff")
+    // };
 
-})
-setInterval(frame,10);
+    // }
+    i=0;
+    function physics(){
+        switch(Player.direction){
+            case 0:
+                break;
+            case 1:
+                Player.x-=Player.vel;
+                break;
+            case 2:
+                Player.x+=Player.vel;
+                break;
+            case 3:
+                Player.y-=Player.vel;
+                break;
+            case 4:
+                Player.y+=Player.vel;
+                break;
+        }
+        
+        if(!i==1){
+            if(Player.x==game.width||Player.x<0)Player.x=-Player.width;
+            if(Player.y==game.height||Player.y<0)Player.y=-Player.height;
+        }
+        i++;
+        
+    }
+        
+    const Player=new Object(0,0,100,1000,1,2,"#ffffff");
+    const game=document.querySelector('#game');
+    const gameContext=game.getContext('2d');
+    function update(){
+        gameContext.clearRect(0,0,game.width,game.height);
+        physics();
+        gameContext.fillStyle="blue";
+        gameContext.fillRect(Player.x,Player.y,Player.width,Player.height);
+        
+        
+    }
+    keysPressed=[];
 
-})
+
+    window.addEventListener('keydown',(e)=>{
+        if(!keysPressed.includes(e.key))keysPressed.push(e.key);
+        // switch(keysPressed){
+        //     case 'left':
+        //         function left(){
+
+        //         }
+        //     break;
+        //     case 'right':
+        //         function left(){
+                    
+        //         }
+        //     break;
+        //     case 'up':
+        //         function left(){
+
+        //         }
+        //     break;
+        //     case 'down':
+        //         function left(){
+                    
+        //         }
+        //     break;
+        // }
+    })
+    window.addEventListener('keyup',(e)=>{
+        // if(!keysPressed.remove
+    })
+setInterval(update,100);
+});
